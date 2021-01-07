bd3e4e034a48835d6dd1769ba6e3693b651398d3
tofujlg <tofuredbull@gmail.com>
2021-01-07 18:29:03 +0100

    fixed a problem with about.js being About.js by itself

---
+++ src/pages/categories.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+import Head from "../components/head"
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <Head title="Categories" />
+      <h1>Categories Page</h1>
+      <p>このページは現在追加中です。</p>
+      <p>しばらくお待ちください。</p>
+    </Layout>
+  )
+}
+
+export default BlogPage
